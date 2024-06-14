0afd6f5c0a69f9773f2d18f2a8733b7b4fb284e5
moodyKfraa <hoodaalkfrawy321@gmail.com>
2024-06-14 18:20:04 +0300

    update

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import logo from "../../assets/logo.png"
 import { NavLink } from 'react-router-dom'
 import styles from "./Styles.module.css"
@@ -16,23 +16,19 @@ function Header({isLoggedIn , changeLang , text}){
                 <li><NavLink to="/contact">{text.ul[1]}</NavLink></li>
                 </ul>
             </nav>
-                {!isLoggedIn?
                     <div className={styles.bts}>
+                    {!isLoggedIn?<Fragment>
                     <NavLink to="/signup">{text.bts[0]}</NavLink>
                     <NavLink to="/login">{text.bts[1]}</NavLink>
+                    </Fragment>
+                    :
+                    <NavLink to="/user" style={{display:"flex"}}><span style={{color:"#eee"}} className="material-symbols-outlined">account_circle</span></NavLink>
+                    }
                     <div className={styles.translate}>
                     <button onClick={()=>changeLang("en")} >en</button>
                     <button onClick={()=>changeLang("ar")} >ar</button>
                     </div>
                     </div>
-                    :
-                    <div className={styles.bts}>
-                    <NavLink to="/user" style={{display:"flex"}}><span style={{color:"#eee"}} className="material-symbols-outlined">account_circle</span></NavLink>
-                        <button onClick={()=>changeLang("en")} >en</button>
-                        <button onClick={()=>changeLang("ar")} >ar</button>
-                    </div>
-
-            }
             </div>
         </div>
     </header>
